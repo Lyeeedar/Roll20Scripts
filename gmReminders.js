9e8745948c915d26db1eb124a7cdd1dbfbaea642
lyeeedar <lyeeedar@gmail.com>
2020-04-07 14:55:01 +0100

    Add support for executing attacks

--- gmReminders.js
+++ gmReminders.js
@@ -1,10 +1,68 @@
 var gmReminders = gmReminders || {};
 
+gmReminders.DoAttack = function(rawattack) {
+    var firstAttack = rawattack.split(" or ")[0];
+    var attacks = firstAttack.split(",");
+
+    for (var i = 0; i < attacks.length; i++) {
+        var attack = attacks[i].trim();
+
+        if (!attack.match(/^\d/)) {
+            attack = "1 " + attack;
+        }
+        
+        var regex = /(.+?) ([^\+]*?) (\+[0-9\/\+]+) \(([^\s]+)( plus ([^)]*))?\)/
+        var match = attack.match(regex);
+
+        var countRaw = match[1];
+        var attackName = match[2];
+        var tohit = match[3];
+        var damage = match[4];
+        var critRange = "20-20";
+        var critMult = "x2";
+        if (damage.includes("/")) {
+            var split = damage.split("/");
+            damage = split[0];
+            critRange = split[1];
+            if (split.length === 3) {
+                critMult = split[2];
+            }
+        }
+
+        var additional = match.length > 5 ? match[6] : "";
+        var additionalParts = additional.split(" and ");
+
+        var count = countRaw.startsWith("+") ? 1 : parseInt(countRaw);
+
+        var atkTemplate = "";
+        for (var c = 0; c < count; c++) {
+            var atk = "<td>hit [[1d20" + tohit + "]]</td>";
+            atk += "<td>dam [[" + damage + "]]";
+           
+            for (var a = 0; a < additionalParts.length; a++) {
+                var part = additionalParts[a];
+
+                var partRegex = /([0-9][^\s]+) (.+)/
+                var partMatch = part.match(partRegex);
+                if (partMatch !== null) {
+                    var roll = partMatch[1];
+                    var type = partMatch[2];
+                    atk += " + [[" + roll + "]] " + type;
+                } else {
+                    atk += " + " + part;
+                }
+            }
+
+            atkTemplate += "<tr>" + atk + "</td></tr>";
+        }
+        sendChat("gmReminder", "/w gm <div class='sheet-rolltemplate-default'><table><caption>" + attack + "</caption> " + atkTemplate + "</table></div>");
+    } 
+};
+
 gmReminders.GenerateNotes = function(CharID) {
     var currChar = getObj("graphic", CharID);
     var gmnotes = currChar.get("gmnotes");
     gmnotes = unescape(gmnotes);
-    log(gmnotes);
 
     if (gmnotes.length === 0) {
         return;
@@ -14,7 +72,6 @@ gmReminders.GenerateNotes = function(CharID) {
     var namematch = gmnotes.match(name);
 
     if (namematch === null) {
-        log("gmReminder: No name found");
         return;
     }
 
@@ -53,7 +110,8 @@ gmReminders.GenerateNotes = function(CharID) {
         
             var melee = gmReminders.GetRegexMatch(line, /Melee.+/);
             if (melee.length > 0) {
-                message += "<li><a style='color:Crimson' title='" + melee + "'>melee</a></li>";
+                var attackButton = "  <a style='background:transparent; color:Red; float:right;padding:0' href='!doattack " + attackButton + "'>Remove</a>";
+                message += "<li><div align='left' style='clear:both'><a style='color:Crimson' title='" + melee + "'>melee</a>" + attackButton + "</div></li>";
             }
         
             var ranged = gmReminders.GetRegexMatch(line, /Ranged.+/);
@@ -105,11 +163,15 @@ on("change:campaign:turnorder", function(args) {
 });
 
 on("chat:message", function(msg) {
-    if (msg.type === "api" && msg.content.startsWith("!gmreminder") && msg.who.includes("(GM)")) {
-        _.each(selected, function (obj){
-            if (obj._type == "graphic") {
-                gmReminders.GenerateNotes(obj._id);
-            }
-        });
+    if (msg.type === "api" && msg.who.includes("(GM)")) {
+        if (msg.content.startsWith("!gmreminder")) {
+            _.each(msg.selected, function (obj){
+                if (obj._type == "graphic") {
+                    gmReminders.GenerateNotes(obj._id);
+                }
+            });
+        } else if (msg.content.startsWith("!doattack")) {
+            gmReminders.DoAttack(msg.content.replace("!doattack ", ""));
+        }
     }
 });
