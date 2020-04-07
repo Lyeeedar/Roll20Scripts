4fb091aedead7e71f04f684dac24831b36c496a8
lyeeedar <lyeeedar@gmail.com>
2020-04-07 15:36:32 +0100

    Improve the look of the reminder. Add support for doing each individual attack. Show crits.

--- gmReminders.js
+++ gmReminders.js
@@ -1,66 +1,111 @@
 var gmReminders = gmReminders || {};
 
+var attackregex = /(.+?) ([^\+]*?) (\+[0-9\/\+]+) \(([^\s]+)( plus ([^)]*))?\)/
 gmReminders.DoAttack = function(rawattack) {
-    var firstAttack = rawattack.split(" or ")[0];
-    var attacks = firstAttack.split(",");
+    try {
+        var firstAttack = rawattack.split(" or ")[0];
+        var attacks = firstAttack.split(",");
 
-    for (var i = 0; i < attacks.length; i++) {
-        var attack = attacks[i].trim();
+        for (var i = 0; i < attacks.length; i++) {
+            var attack = attacks[i].trim();
 
-        if (!attack.match(/^\d/)) {
-            attack = "1 " + attack;
-        }
-        
-        var regex = /(.+?) ([^\+]*?) (\+[0-9\/\+]+) \(([^\s]+)( plus ([^)]*))?\)/
-        var match = attack.match(regex);
-
-        var countRaw = match[1];
-        var attackName = match[2];
-        var tohit = match[3];
-        var damage = match[4];
-        var critRange = "20-20";
-        var critMult = "x2";
-        if (damage.includes("/")) {
-            var split = damage.split("/");
-            damage = split[0];
-            critRange = split[1];
-            if (split.length === 3) {
-                critMult = split[2];
+            if (!attack.match(/^\d/)) {
+                attack = "1 " + attack;
             }
-        }
+            
+            var match = attack.match(attackregex);
 
-        var additional = match.length > 5 ? match[6] : "";
-        var additionalParts = additional.split(" and ");
+            var countRaw = match[1];
+            var attackName = match[2];
+            var tohit = match[3];
+            tohit = tohit.split("/");
 
-        var count = countRaw.startsWith("+") ? 1 : parseInt(countRaw);
+            var damage = match[4];
+            var critRange = "20-20";
+            var critMult = "x2";
+            if (damage.includes("/")) {
+                var split = damage.split("/");
+                damage = split[0];
 
-        var atkTemplate = "";
-        for (var c = 0; c < count; c++) {
-            var atk = "<td>hit [[1d20" + tohit + "]]</td>";
-            atk += "<td>dam [[" + damage + "]]";
-           
-            for (var a = 0; a < additionalParts.length; a++) {
-                var part = additionalParts[a];
-
-                var partRegex = /([0-9][^\s]+) (.+)/
-                var partMatch = part.match(partRegex);
-                if (partMatch !== null) {
-                    var roll = partMatch[1];
-                    var type = partMatch[2];
-                    atk += " + [[" + roll + "]] " + type;
+                if (split[1].includes("-")) {
+                    critRange = split[1];
                 } else {
-                    atk += " + " + part;
+                    critMult = split[1];
+                }
+
+                if (split.length === 3) {
+                    critMult = split[2];
                 }
             }
+            var critvalue = critRange.split("-")[0];
 
-            atkTemplate += "<tr>" + atk + "</td></tr>";
+            var additional = match.length > 5 ? match[6] : "";
+            var additionalParts = additional.split(" and ");
+
+            var count = countRaw.startsWith("+") ? 1 : parseInt(countRaw);
+            if (count < tohit.length) {
+                count = tohit.length;
+            }
+
+            var atkTemplate = "";
+            for (var c = 0; c < count; c++) {
+                var hit = c >= tohit.length ? tohit[tohit.length-1] : tohit[c];
+
+                var atk = "<td>" + hit + " hit [[1d20" + hit + "cs>" + critvalue + "]]</td><td>dam [[" + damage + "]]";
+            
+                for (var a = 0; a < additionalParts.length; a++) {
+                    var part = additionalParts[a];
+
+                    var partRegex = /([0-9][^\s]+) (.+)/
+                    var partMatch = part.match(partRegex);
+                    if (partMatch !== null) {
+                        var roll = partMatch[1];
+                        var type = partMatch[2];
+                        atk += " + [[" + roll + "]] " + type;
+                    } else {
+                        atk += " + " + part;
+                    }
+                }
+
+                atkTemplate += "<tr>" + atk + "</td></tr>";
+                atkTemplate += "<tr><td>crit hit [[1d20" + hit + "cs>" + critvalue + "]]</td><td>crit dam [[" + damage + "*" + critMult.substr(1) + "]]</td></tr>";
+            }
+            sendChat("gmReminder", "/w gm <div class='sheet-rolltemplate-default'><table><caption>" + attack + "</caption> " + atkTemplate + "</table></div>");
         }
-        sendChat("gmReminder", "/w gm <div class='sheet-rolltemplate-default'><table><caption>" + attack + "</caption> " + atkTemplate + "</table></div>");
-    } 
+    } catch(err) {
+        sendChat("gmReminder", "/w gm Failed to execute attack '" + rawattack + "' due to " + err.message);
+    }
+};
+
+gmReminders.BuildAttackButtons = function(attackraw) {
+    var attacks = attackraw.split(" or ");
+
+    var output = "";
+
+    for (var i = 0; i < attacks.length; i++) {
+        var attack = attacks[i];
+
+        var adjustedAttack = attack;
+        if (!adjustedAttack.match(/^\d/)) {
+            adjustedAttack = "1 " + adjustedAttack;
+        }
+        var match = adjustedAttack.match(attackregex);
+        var attackname = match[2];
+        var namesplit = attackname.split(" ");
+        var shortname = namesplit[namesplit.length-1];
+
+        output +=  "  <a style='background:transparent; color:Red; padding:0; padding-right:5px' title='" + attack + "' href='!doattack " + attack + "'>" + shortname + "</a>";
+    }
+
+    return output;
 };
 
 gmReminders.GenerateNotes = function(CharID) {
     var currChar = getObj("graphic", CharID);
+    if (currChar === undefined) {
+        return;
+    }
+
     var gmnotes = currChar.get("gmnotes");
     gmnotes = unescape(gmnotes);
 
@@ -98,30 +143,27 @@ gmReminders.GenerateNotes = function(CharID) {
             if (match !== null) {
                 message += "<li>Fast Healing: " + match[1] + "</li>";
             }
-        
-            var defenses = "";
-            defenses += gmReminders.GetRegexMatch(line, /DR[^;]+/);
-            defenses += gmReminders.GetRegexMatch(line, /Resist[^;]+/);
-            defenses += gmReminders.GetRegexMatch(line, /Immune[^;]+/);
-            defenses += gmReminders.GetRegexMatch(line, /SR[^;]+/);
-            if (defenses.length > 0) {
-                message += "<li><a style='color:DeepSkyBlue' title='" + defenses + "'>defenses</a></li>";
+
+            if (line.startsWith("AC")) {
+                message += "<li><a style='color:DeepSkyBlue' title='" + line + "'>AC</a></li>";
             }
         
-            var melee = gmReminders.GetRegexMatch(line, /Melee.+/);
-            if (melee.length > 0) {
-                var attackButton = "  <a style='background:transparent; color:Red; float:right;padding:0' href='!doattack " + attackButton + "'>Remove</a>";
-                message += "<li><div align='left' style='clear:both'><a style='color:Crimson' title='" + melee + "'>melee</a>" + attackButton + "</div></li>";
+            if (line.startsWith("Defensive Abilities")) {
+                message += "<li><a style='color:DeepSkyBlue' title='" + line + "'>Defenses</a></li>";
             }
         
-            var ranged = gmReminders.GetRegexMatch(line, /Ranged.+/);
-            if (ranged.length > 0) {
-                message += "<li><a style='color:Crimson' title='" + ranged + "'>ranged</a></li>";
+            if (line.startsWith("Melee")) {
+                var attackButton = gmReminders.BuildAttackButtons(line.replace("Melee ", ""));
+                message += "<li><div align='left' style='clear:both'>Melee:" + attackButton + "</div></li>";
+            }
+        
+            if (line.startsWith("Ranged")) {
+                var attackButton = gmReminders.BuildAttackButtons(line.replace("Ranged ", ""));
+                message += "<li><div align='left' style='clear:both'>Ranged:" + attackButton + "</div></li>";
             }
 
-            var aura = gmReminders.GetRegexMatch(line, /Aura.+/);
-            if (aura.length > 0) {
-                message += "<li><a style='color:DarkOrange' title='" + aura + "'>aura</a></li>";
+            if (line.startsWith("Aura")) {
+                message += "<li><a style='color:DarkOrange' title='" + line + "'>Aura</a></li>";
             }
         }
     }
