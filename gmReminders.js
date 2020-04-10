f97d39fbf18fa61c92e2de7330c63bd42a6adc4f
lyeeedar <lyeeedar@gmail.com>
2020-04-10 12:00:55 +0100

    Improve the rolling by doing the roll evaluation/execution manually

--- gmReminders.js
+++ gmReminders.js
@@ -2,6 +2,124 @@ var gmReminders = gmReminders || {};
 gmReminders.customMessageMap = {};
 gmReminders.guid = 0;
 
+gmReminders.DoRollArbitraryDice = function (roll) {
+	var result = gmReminders.EvaluateDiceExpression(roll);
+
+	var extraMessage = "";
+	if (result.hadCritHit) {
+		extraMessage = "  CRITICAL HIT!";
+	} else if (result.hadCritMiss) {
+		extraMessage = "  FUMBLE!";
+	}
+
+	sendChat("gmReminders", `/direct <div>${gmReminders.FormatRoll(result)}${extraMessage}</div>`)
+}
+
+gmReminders.EvaluateDiceExpression = function (roll) {
+	log(roll);
+
+	let diceRegex = /[0-9]+d[0-9]+(>[0-9]+)?/
+
+	let remainingRoll = roll;
+
+	let hadCritHit = false;
+	let hadCritMiss = false;
+
+	let rolledDisplayString = "";
+	let rolledString = "";
+	while (remainingRoll != null && remainingRoll.length > 0) {
+		let match = remainingRoll.match(diceRegex);
+		if (match !== null) {
+			let split = remainingRoll.split(diceRegex);
+			rolledString += split[0];
+			rolledDisplayString += split[0];
+
+			let result = gmReminders.RollDice(match[0]);
+			rolledString += result.total;
+
+			let critResult = 20;
+			if (match[1] != undefined) {
+				critResult = parseInt(match[1].substr(1));
+			}
+
+			rolledDisplayString += "(";
+			for (var i = 0; i < result.rolledValues.length; i++) {
+				if (i > 0) {
+					rolledDisplayString += "+";
+				}
+
+				let rolledValue = result.rolledValues[i];
+
+				if (rolledValue == 1) {
+					hadCritMiss = true;
+					rolledDisplayString += `${rolledValue}!`;
+				} else if (rolledValue >= critResult) {
+					hadCritHit = true;
+					rolledDisplayString += `${rolledValue}!`;
+				} else {
+					rolledDisplayString += `${rolledValue}`;
+				}
+			}
+			rolledDisplayString += ")";
+			
+			remainingRoll = remainingRoll.substr(split[0].length + match[0].length);
+		} else {
+			rolledString += remainingRoll;
+			rolledDisplayString += remainingRoll;
+			break;
+		}
+	}
+
+	let finalValue = eval(rolledString);
+
+	roll = roll.replace("*", "x");
+	rolledDisplayString = rolledDisplayString.replace("*", "x");
+	return {
+		originalRoll: roll,
+		afterRolling: rolledDisplayString,
+		result: finalValue,
+		hadCritMiss: hadCritMiss,
+		hadCritHit: hadCritHit
+	}
+};
+
+gmReminders.RollDice = function (diceRoll) {
+	let split = diceRoll.split("d");
+	let count = parseInt(split[0]);
+	let dice = parseInt(split[1]);
+	
+	let rolledValues = [];
+
+	let value = 0;
+	for (var i = 0; i < count; i++) {
+		let val = randomInteger(dice);
+		rolledValues.push(val);
+		value += val;
+	}
+
+	return {
+		total: value,
+		rolledValues: rolledValues
+	};
+}
+
+gmReminders.FormatRoll = function(rollResult) {
+	var borderCol = "goldenrod";
+	if (rollResult.hadCritHit) {
+		borderCol = "green";
+	} else if (rollResult.hadCritMiss) {
+		borderCol = "red";
+	}
+
+	var spanStyle = `style='border:2px solid ${borderCol};background-color:yellow;padding:0.5px 2px 0.5px 2px;cursor:help'`;
+
+	var output = `<span ${spanStyle} title='Rolling ${rollResult.originalRoll} = ${rollResult.afterRolling}'>`
+	output += `${rollResult.result}`
+	output += `</span>`
+
+	return output;
+};
+
 var attackregex = /(.+?) ([^\+]*?) (\+[0-9\/\+]+) \(([^\s]+)( plus ([^)]*))?\)/;
 gmReminders.DoAttack = function (rawattack) {
 	try {
@@ -52,22 +170,34 @@ gmReminders.DoAttack = function (rawattack) {
 
 			var atkTemplate = "";
 			for (var c = 0; c < count; c++) {
-				var hit =
-					c >= tohit.length ? tohit[tohit.length - 1] : tohit[c];
+				var hit = c >= tohit.length ? tohit[tohit.length - 1] : tohit[c];
 
-				var atk =
-					"<td>" +
-					hit +
-					" hit [[1d20" +
-					hit +
-					"cs>" +
-					critvalue +
-					"]]</td><td>dam [[" +
-					damage +
-					"]]";
+				var hitRoll = `1d20>${critvalue}${hit}`;
+				var hitRollResult = gmReminders.EvaluateDiceExpression(hitRoll);
+
+				var atk = `<td>`;
+				atk += `${hit} hit ${gmReminders.FormatRoll(hitRollResult)}`;
+
+				if (hitRollResult.hadCritHit) {
+					var critRollResult = gmReminders.EvaluateDiceExpression(hitRoll);
+					atk += `!!${gmReminders.FormatRoll(critRollResult)}`;
+				}
+				atk += `</td>`;
+
+				var damRollResult = gmReminders.EvaluateDiceExpression(damage);
+				atk += `<td>dam ${gmReminders.FormatRoll(damRollResult)}`;
+				if (hitRollResult.hadCritHit) {
+					var mult = parseInt(critMult.substr(1))-1;
+
+					var critRollResult = gmReminders.EvaluateDiceExpression(`${damRollResult.result}+(${damage})*${mult}`);
+					atk += `!!${gmReminders.FormatRoll(critRollResult)}`;
+				}
 
 				for (var a = 0; a < additionalParts.length; a++) {
 					var part = additionalParts[a];
+					if (part == undefined || part.length == 0) {
+						continue;
+					}
 
 					var partRegex = /([0-9][^\s]+) (.+)/;
 					var partMatch = part.match(partRegex);
@@ -81,16 +211,6 @@ gmReminders.DoAttack = function (rawattack) {
 				}
 
 				atkTemplate += "<tr>" + atk + "</td></tr>";
-				atkTemplate +=
-					"<tr><td>crit hit [[1d20" +
-					hit +
-					"cs>" +
-					critvalue +
-					"]]</td><td>crit dam [[" +
-					damage +
-					"*" +
-					critMult.substr(1) +
-					"]]</td></tr>";
 			}
 			sendChat(
 				"gmReminder",
@@ -829,6 +949,8 @@ on("chat:message", function (msg) {
 				});
 			} else if (msg.content.startsWith("!removereminder")) {
 				gmReminders.RemoveCustomReminder(msg.content.replace("!removereminder ", ""));
+			} else if (msg.content.startsWith("!dodice")) {
+				gmReminders.DoRollArbitraryDice(msg.content.replace("!dodice ", ""));
 			}
 		}
 	} catch (ex) {
